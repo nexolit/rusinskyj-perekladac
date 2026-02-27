e1575fdbadcd2f9c2965bd2de92e0939b754a09a
Michal Popadič <michaylo@rusyn.it>
2026-02-27 01:33:47 +0100

    Added info page

---
+++ app/src/pages/Info.vue
@@ -0,0 +1,23 @@
+<script setup>
+const icon = new URL('../assets/icons/icon-512.png', import.meta.url).href;
+</script>
+
+<template>
+<section class="section is-flex is-flex-direction-column is-align-items-center">
+    <h1 class="title center is-size-2">Rusínsky prekladač</h1>
+    <section class="section columns is-vcentered is-variable is-8">
+      <figure class="column is-full-mobile is-half-tablet has-text-centered">
+        <img :src="icon" alt="Logo Rusínského prekladača" style="max-width: 100%; height: auto;" />
+      </figure>
+
+      <div class="column is-full-mobile is-half-tablet">
+        <p class="is-size-4">
+          Tento projekt vznikol ku výročiu 30. výročia kodifikácie Rusínského jazyka, <br />
+          aby sa zachoval a neupadol do zabudnutia, <br />
+          aby nezmizol z verejného povedomia, <br />
+          a aby zostal súčasťou našej kultúrnej identity.
+        </p>
+      </div>
+    </section>
+</section>
+</template>
--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Domov from './pages/Domov.vue'
 import Preklad from './pages/Preklad.vue'
+import Info from './pages/Info.vue'
 
 const routes = [
   { path: '/', component: Domov },
-  { path: '/preklad', component: Preklad }
+  { path: '/preklad', component: Preklad },
+  { path: '/info', component: Info }
 ]
 
 export default createRouter({
