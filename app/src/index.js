0ef3360aa6e93e6f16d31f6316d3fbeb15163554
Michal Popadič <michaylo@rusyn.it>
2026-04-12 22:47:24 +0200

    Added fontawesome icon pack

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bulma/css/bulma.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import router from './router'
 
 // Mount app
